bcbcd85c6e2293ddb1987388f8af6808050d4c05
somayya65 <113750427+somayya65@users.noreply.github.com>
2022-09-25 11:34:15 +0530

    Update nexus_centos.sh

--- Nexus/nexus_centos.sh
+++ Nexus/nexus_centos.sh
@@ -1,13 +1,13 @@
 yum install wget -y && yum install unzip -y && yum install java -y &&
 
-wget https://download.sonatype.com/nexus/oss/nexus-2.14.4-03-bundle.zip &&
+wget https://download.sonatype.com/nexus/oss/nexus-2.15.1-02-bundle.zip &&
 
-unzip nexus-2.14.4-03-bundle.zip &&
+unzip nexus-2.15.1-02-bundle.zip &&
 
-rm nexus-2.14.4-03/bin/nexus &&
+rm nexus-2.15.1-02/bin/nexus &&
 
-cp nexus nexus-2.14.4-03/bin/ &&
+cp nexus nexus-2.15.1-02/bin/ &&
 
-chmod +x nexus-2.14.4-03/bin/nexus
+chmod +x nexus-2.15.1-02/bin/nexus
 
-bash nexus-2.14.4-03/bin/nexus restart
+bash nexus-2.15.1-02/bin/nexus restart
